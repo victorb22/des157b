4da7dbff6dd508143c83f9b7bbbec39c413ab6a4
Victor Barajas <vbaraja7@scc2102-25.ucdavis.edu>
2025-04-03 15:45:41 -0700

    1

---
+++ script.js
@@ -0,0 +1,47 @@
+(function() {
+    'use strict';
+
+    const button = document.querySelector('button');
+    const body = document.querySelector('body');
+    const banner = document.querySelector('#banner');
+    const sections = document.querySelectorAll('section')
+    const cream = document.querySelector('#cream');
+    const orange = document.querySelector('#orange');
+    const dgreen = document.querySelector('#dgreen');
+    const green = document.querySelector('#green');
+    const black = document.querySelector('#black');
+    const bannerText = document.querySelector('#textBanner');
+    let mode = 'dark';
+
+    button.addEventListener('click', function() {
+        if (mode === 'dark') {
+            body.className = 'switch';
+            banner.className = 'switch';
+            button.className = 'switch';
+            bannerText.className = 'switch';
+            cream.className = 'switch';
+            orange.className = 'switch';
+            dgreen.className = 'switch';
+            green.className = 'switch';
+            black.className = 'switch';
+            for (const section of sections) {
+                section.className = 'switch';
+            }
+            mode = 'light';
+        } else {
+            body.removeAttribute('class');
+            banner.removeAttribute('class');
+            button.removeAttribute('class');
+            banner.removeAttribute('class');
+            cream.removeAttribute('class');
+            orange.removeAttribute('class');
+            dgreen.removeAttribute('class');
+            green.removeAttribute('class');
+            black.removeAttribute('class');
+            for (const section of sections) {
+                section.removeAttribute('class');
+            }
+            mode = 'dark'
+        }
+    })
+})()
