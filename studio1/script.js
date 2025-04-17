25db32fb3642fc7eec9146cbaf877854ed27aec7
Victor Barajas <victorbarajas@Victors-Air-7.attlocal.net>
2025-04-17 10:44:21 -0700

    link-video

--- studio1/script.js
+++ studio1/script.js
@@ -2,5 +2,35 @@
     'use strict';
     console.log('reading js');
 
+    const myVideo = document.querySelector('#myVideo');
+    const line1 = document.querySelector('#line1');
+    const line2 = document.querySelector('#line2');
+    const line3 = document.querySelector('#line3');
+
+    const poem = {
+    start: [0, 5, 8],
+    stop: [4, 7, 10],
+    line: [line1, line2, line3]
+    }
+
+    const intervalID = setInterval(checkTime, 1000);
+
+    function checkTime() {
+        console.log(parseInt(myVideo.currentTime));
+      
+        for (let i = 0; i < poem.start.length; i++) {
+          if (poem.start[i] < myVideo.currentTime && myVideo.currentTime < poem.stop[i]) {
+            poem.line[i].className = "showing";
+          } else {
+            poem.line[i].className = "hidden";
+          }
+        }
+      }
+
     
+      myVideo.addEventListener('mousemove', function(event) {
+        var saturation = event.clientX/ window.innerWidth;
+        myVideo.style.filter = "saturate(" + saturation + ")";
+    });
+      
 }());
